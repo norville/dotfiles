91805c0c88e18cca8ada5b570df8105169c1aa27
norville <ing.norville@gmail.com>
2017-03-05 15:21:23 +0100

    Add vim plugin install

--- update_dev_env.sh
+++ update_dev_env.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Update Homebrew
 brew update
 brew upgrade
 
+# Update PyPI
 pip3 install --upgrade pip wheel setuptools powerline-status
+
+# Update NPM
 npm update -g
 
+# Update Ruby
 LATEST_RUBY=`egrep "^\s+\d\.\d\.\d+$" <(rbenv install -l) | tail -1`
 GLOBAL_RUBY=$(rbenv global)
-
 if [ $LATEST_RUBY==$GLOBAL_RUBY ]
 then
 	echo "Ruby is already up to date."
@@ -18,4 +22,7 @@ else
 	rbenv rehash
 	rbenv global $LATEST_RUBY
 fi
+
+# Update VIM Plugins
+vim +PluginInstall +qall
 
