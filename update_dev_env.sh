72fe9b084d50e8abb00fb48cf69c47b30b05e956
norville <ing.norville@gmail.com>
2017-03-05 15:14:11 +0100

    fix rbenv update

--- update_dev_env.sh
+++ update_dev_env.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-brew upgrade
 brew update
+brew upgrade
 
 pip3 install --upgrade pip wheel setuptools powerline-status
 npm update -g
 
 LATEST_RUBY=`egrep "^\s+\d\.\d\.\d+$" <(rbenv install -l) | tail -1`
-rbenv install $LATEST_RUBY
-rbenv init
-rbenv rehash
-rbenv global $LATEST_RUBY
+GLOBAL_RUBY=$(rbenv global)
+
+if [ $LATEST_RUBY==$GLOBAL_RUBY ]
+then
+	echo "Ruby is already up to date."
+else
+	rbenv install $LATEST_RUBY
+	rbenv init
+	rbenv rehash
+	rbenv global $LATEST_RUBY
+fi
+
