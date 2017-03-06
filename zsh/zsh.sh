e04936e4ac0c7baa3428508d2451a939383445f7
norville <ing.norville@gmail.com>
2017-03-06 09:22:23 +0100

    fix setting shell in zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -7,8 +7,7 @@ then
 	echo "ZSH is already the login shell."
 else
 	echo "Changing login shell to ZSH..."
-	sudo echo $ZSH_PATH >> /etc/shells
+	echo $ZSH_PATH | sudo tee -a /etc/shells
 	chsh -s $ZSH_PATH
-#	export ~/.zshrc
 fi
 
