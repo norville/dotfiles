5a64a2fbb169e583283496d2a35a5aec9066d6ea
norville <ing.norville@gmail.com>
2019-04-15 03:55:55 +0200

    begin implemeting bootstrap

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -38,3 +38,92 @@ dfconf config status.showUntrackedFiles no
 # TODO
 # - install nerd font
 # - set gruvbox theme for terminal (mac/linux)
+#!/bin/bash
+
+set -e
+
+# Include customised echo functions
+#source ./install.d/echoes.sh
+
+# Define variables
+USR=$(whoami)
+
+# Ask for the administrator password upfront
+sudo -v
+
+# If system is macOS
+if [[ $(uname -s) == 'Darwin' ]]; then
+
+    BREWFILE=~/dev/dotfiles/macos/Brewfile
+
+    # Check for Homebrew and install if we don't have it, else update it
+    if [[ ! $(which brew) ]]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        try "Cannot install Homebrew."
+        brew bundle -v --file=$BREWFILE
+    else
+        if [[ $(brew bundle cleanup --file=$BREWFILE) ]]; then
+            read -r -p "Do you want to uninstall all extra formulae? [y|n] " response
+            if [[ $response =~ (yes|y|Y) ]]; then
+                brew bundle cleanup --file=$BREWFILE --force
+            fi
+        fi
+        brew update --force
+        brew upgrade
+        brew cask upgrade #--greedy
+    fi
+    brew cleanup
+    brew doctor
+
+    # Update PIP base packages
+    pip3 install -U pip setuptools wheel
+
+    #TODO install apps via Mac App Store
+
+# If system is Linux
+elif [[ $(uname -s) == 'Linux' ]]; then
+
+    APT_FILE=~/dev/dotfiles/install.d/apt-packages
+
+    # Install rquired apps
+    sudo apt-get update
+    xargs -a $APT_FILE sudo apt-get install -y
+    if [[ $? != 0 ]]; then
+        exit 2
+    fi
+
+    # Cleanup
+    sudo apt autoremove
+    sudo apt autoclean
+fi
+
+# Set ZSH as login shell
+ZSH_PATH=$(which zsh)
+if [[ $SHELL != $ZSH_PATH ]]; then
+    echo $ZSH_PATH | sudo tee -a /etc/shells
+    sudo chsh -s $ZSH_PATH $USR
+#else
+    #bot "ZSH is already your login shell."
+fi
+
+# Install Antigen
+if [[ -d ~/.antigen ]]; then
+    rm -rf ~/.antigen
+fi
+mkdir -p ~/.antigen
+curl -sL git.io/antigen > ~/.antigen/antigen.zsh
+
+# Download Docker Completion file
+if [[ ! -d ~/.zsh_completion ]]; then
+    mkdir -p ~/.zsh_completion
+    curl -sL https://raw.githubusercontent.com/docker/machine/v0.14.0/contrib/completion/zsh/_docker-machine > ~/.zsh_completion/_docker-machine
+fi
+
+# Install Vundle plugins
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+vim -i NONE -c VundleUpdate -c quitall > /dev/null 2>&1
+
+# Install required Python packages
+#pip3 install -r install.d/requirements.txt
