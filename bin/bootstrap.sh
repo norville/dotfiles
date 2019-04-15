11caf1919a063c252e57ddf7143bb3c34c584bfd
norville <ing.norville@gmail.com>
2019-04-15 03:21:09 +0200

    fix attributes, add bootstrap

---
+++ bin/bootstrap.sh
@@ -0,0 +1,40 @@
+
+### Bootstrap script
+
+# TODO install git
+
+REPO_URL=https://github.com/norville/dotfiles.git   # URL to remote repo
+BARE_DIR=.dfbare                                    # git dir for bare repo
+BKP_DIR=$HOME/.dfbackup                             # backup dir for pre-existing dotfiles
+
+# Define function to automatically run git against the bare repo in $HOME
+function dfconf {
+    # /usr/bin/git --git-dir=<git-bare-dir> --work-tree=<dotfiles-dir>'
+    /usr/bin/git --git-dir=$BARE_DIR --work-tree=$HOME $@
+}
+
+# Clone remote repo into the bare repo inside $HOME
+# git clone --bare <git-repo-url> <git-bare-dir>
+cd $HOME && git clone --bare $REPO_URL $BARE_DIR
+
+# Checkout bare repo
+dfconf checkout
+
+# If checkout fails
+# TODO fix if condition
+if $?; then
+    # Create backup dir
+    mkdir -p $BKP_DIR
+    # Move pre-existing dotfiles to $BKP_DIR
+    dfconf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BKP_DIR/{}
+    dfconf checkout
+fi;
+
+# Config bare repo to ignore untracked files
+dfconf config status.showUntrackedFiles no
+
+# TODO continue bootstrap
+
+# TODO
+# - install nerd font
+# - set gruvbox theme for terminal (mac/linux)
