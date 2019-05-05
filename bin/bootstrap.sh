587d5a5feb984dd98aaf46550c79da2d25abd1a3
norville <ing.norville@gmail.com>
2019-05-05 16:43:39 +0200

    define bootstrap sections, install base software

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,63 +1,200 @@
 #!/bin/bash
 
-set -e
+### Define variables and functions
 
-# Define variables
 REPO_URL=https://github.com/norville/dotfiles.git   # URL to remote repo
 BARE_DIR=.dfbare                                    # git dir for bare repo
 BKP_DIR=$HOME/.dfbackup                             # backup dir for pre-existing dotfiles
 
-# Define function to automatically run git against the bare repo in $HOME
+# Check last exit status and print message
+function exit_status() {
+
+    if [[ $? != 0 ]]; then
+        # Print error message and exit
+        echo "Error $1"
+        exit 2
+    else
+        # Print success message
+        echo "Done $1"
+    fi
+
+}
+
+# Automatically run git against the bare repo in $HOME
 function dfconf {
+
     # /usr/bin/git --git-dir=<git-bare-dir> --work-tree=<dotfiles-dir>
     /usr/bin/git --git-dir=$BARE_DIR --work-tree=$HOME $@
+
 }
+
+# Ensure bare repo is configured
+function prep_repo {
+
+    # Check bare repo dir
+    cd $HOME
+    if [[ ! -d $BARE_DIR ]]; then
+        # Create directory for bare repository
+        mkdir -p $BARE_DIR
+    fi
+
+    # Check bare repo status
+    cd $BARE_DIR
+    if [[ ! $(git rev-parse --is-bare-repository) ]]; then
+
+        # Clone remote repo into the bare repo inside $HOME
+        # git clone --bare <git-repo-url> <git-bare-dir>
+        cd $HOME
+        git clone --bare $REPO_URL $BARE_DIR
+        exit_status "cloning bare repo into $BARE_DIR"
+
+        # Checkout bare repo
+        dfconf checkout
+        # If checkout fails
+        if [[ $? != 0 ]]; then
+            # Create backup dir
+            mkdir -p $BKP_DIR
+            # Move pre-existing dotfiles to $BKP_DIR
+            dfconf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BKP_DIR/{}
+            dfconf checkout
+        fi
+
+        # Config bare repo to ignore untracked files
+        dfconf config status.showUntrackedFiles no
+
+    fi
+
+    # Ensure current dir is $HOME
+    cd $HOME
+
+}
+
+### Bootstrap begin
+
+#set -e
 
 # Get admin privileges
 sudo -v
 
-# If system is Linux, ensure git is installed
+### Prepare bare repo and install/update required software
+
+### Detect system
 if [[ $(uname -s) == 'Linux' ]]; then
+
+    ### If system is Linux
+
+    # Update APT repos
     sudo apt-get update
+    exit_status "updating APT repos"
+
+    # Ensure git is installed
     sudo apt-get install git -y
-fi
+    exit_status "installing git"
 
-# Check bare repo dir
-cd $HOME
-if [[ ! -d $BARE_DIR ]]; then
+    # Prepare bare repo before continuing
+    prep_repo
 
-    # Create directory for bare repository
-    mkdir -p $BARE_DIR
+    # Install required packages
+    APT_PKGS=~/.dfconf/apt-packages
+    xargs -a $APT_PKGS sudo apt-get install -y
+    exit_status "installing required packages in $APT_PKGS"
 
-fi
+    # Cleanup
+    sudo apt autoremove
+    sudo apt autoclean
 
-# Check bare repo status
-cd $BARE_DIR
-if [[ ! $(git rev-parse --is-bare-repository) ]]; then
+elif [[ $(uname -s) == 'Darwin' ]]; then
 
-    # Clone remote repo into the bare repo inside $HOME
-    # git clone --bare <git-repo-url> <git-bare-dir>
-    cd $HOME
-    git clone --bare $REPO_URL $BARE_DIR
+    ### If system is macOS
 
-    # Checkout bare repo
-    dfconf checkout
+    # Prepare bare repo before continuing
+    prep_repo
 
-    # If checkout fails
-    if [[ $? != 0 ]]; then
-        # Create backup dir
-        mkdir -p $BKP_DIR
-        # Move pre-existing dotfiles to $BKP_DIR
-        dfconf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BKP_DIR/{}
-        dfconf checkout
+    # Load required packages
+    BREW_PKGS=~/.dfconf/Brewfile
+
+    # Check for Homebrew
+    if [[ ! $(which brew) ]]; then
+
+        # Install Homebrew and all required packages
+
+        BREW_URL='https://raw.githubusercontent.com/Homebrew/install/master/install'
+        /usr/bin/ruby -e "$(curl -fsSL $BREW_URL)"
+        exit_status "installing Homebrew"
+
+        brew bundle -v --file=$BREW_PKGS
+        exit_status "installing required packages"
+
+    else
+
+        # Update Homebrew and all required packages
+
+        # Check for extra packages
+        if [[ $(brew bundle cleanup --file=$BREW_PKGS) ]]; then
+            read -r -p "Do you want to uninstall all extra formulae? [y|n] " response
+            if [[ $response =~ (yes|y|Y) ]]; then
+                # Remove extra packages
+                brew bundle cleanup --file=$BREW_PKGS --force
+                exit_status "removing extra packages"
+            fi
+        fi
+
+        brew update --force
+        exit_status "updating Homebrew"
+
+        brew upgrade
+        exit_status "upgrading Homebrew formulae"
+
+        brew cask upgrade #--greedy
+        exit_status "upgrading Homebrew caks"
+
     fi
 
-    # Config bare repo to ignore untracked files
-    dfconf config status.showUntrackedFiles no
+    # Cleanup
+    brew cleanup
+    brew doctor
+
+    # Update PIP base packages
+    pip3 install -U pip setuptools wheel
+    exit_status "updating PIP packages"
+
+    # TODO
+    # - install apps via Mac App Store
+    # - restore preferences (Mackup / Mathias)
+
 fi
 
-# TODO continue bootstrap
+### Configure environment
+
+### Shell and plugins
+
+# Ensure ZSH is the login shell
+ZSH_PATH=$(which zsh)
+if [[ $SHELL != $ZSH_PATH ]]; then
+
+    # Set ZSH as login shell
+    echo $ZSH_PATH | sudo tee -a /etc/shells
+    sudo chsh -s $ZSH_PATH $USR
+    exit_status "setting ZSH as login shell"
+
+else
+
+    # Already set!
+    exit_status "checking login shell (ZSH)"
+
+fi
+
+# Antigen
+
+# ZSH Completions
+
+### Editor and plugins
+
+# Vundle
 
 # TODO
 # - install nerd font
 # - set gruvbox theme for terminal (mac/linux)
+
+### Bootstrap end
+
