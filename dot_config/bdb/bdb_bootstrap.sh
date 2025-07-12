44093dab32805ef40f3d20f4381d10e680208c85
norville <ing.norville@protonmail.com>
2025-07-12 09:12:27 +0000

    move helper functions to separate file

--- dot_config/bdb/bdb_bootstrap.sh
+++ dot_config/bdb/bdb_bootstrap.sh
@@ -3,98 +3,28 @@
 
 ### BDF BOOT - bootstrap Bassa's Dot Files
 
+# Source helper functions (portable, works regardless of current directory)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/bdb_helpers.sh"
+
+# Ensure strict error handling
+# -e: exit on error
+# -u: treat unset variables as an error
+# -o pipefail: return the exit status of the last command in the pipeline that failed
+# -x: print commands and their arguments as they are executed (for debugging)
 set -euo pipefail
 
-# Ensure download of entire script
-global_bootstrap() {
+# Trap errors and call handler
+trap 'bdb_handle_error' ERR
 
-    # Define global variables
+# Define function to ensure download of entire script
+bdb_bootstrap() {
+
+    # Define variables
     GITHUB_USER="norville"  # GitHub username for dotfiles repo
     PLATFORM=""             # OS type (Darwin/Linux)
     DISTRO=""               # OS distribution name
-
-    # Escape sequences for colored output (ANSI C quoting)
-    RST_COL=$'\033[39;49;00m'   # reset
-    RED_COL=$'\033[31;01m'      # red
-    GRN_COL=$'\033[32;01m'      # green
-    BLU_COL=$'\033[34;01m'      # blue
-    YLW_COL=$'\033[33;01m'      # yellow
-    CYN_COL=$'\033[36;01m'      # cyan
-    MGN_COL=$'\033[35;01m'      # magenta
-    WHT_COL=$'\033[97;01m'      # white
-
-    # --- Output helper functions ---
-
-    # Print success message (green)
-    bdb_success() {
-        printf "\n%s|vvv| SUCCESS %s. %s" "${GRN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print error message (red)
-    bdb_error() {
-        printf "\n%s|xxx| ERROR %s %s" "${RED_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print alert message (yellow)
-    bdb_alert() {
-        printf "\n%s|!!!| %s %s" "${YLW_COL}" "$1" "${RST_COL}"
-    }
-
-    # Print info message - begin (white)
-    bdb_info_in() {
-        printf "%s\n|BDB| +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n|BDB|\n|BDB| %s.\n|BDB|%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print info message (white)
-    bdb_info() {
-        printf "%s\n|BDB|\n|BDB| %s.\n|BDB|%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print info message - end (white)
-    bdb_info_out() {
-        printf "%s\n|BDB|\n|BDB| %s.\n|BDB|\n|BDB| ---------------------------------------------------------------------------------%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print run message (blue)
-    bdb_run() {
-        printf "\n%s|###| %s...%s" "${BLU_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print outcome message (cyan)
-    bdb_outcome() {
-        printf " %s%s.%s" "${CYN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Print message before command (cyan)
-    bdb_command() {
-        printf "\n%s|>>>| %s:%s" "${CYN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    }
-
-    # Ask user yes/no, return 0 for yes (default: no)
-    bdb_ask() {
-        printf "%s\n|???|\n|???| %s ? [y|N] >\n|???| %s" "${MGN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-        read -s -r -n 1 response
-        if [[ ${response:-} =~ (y|Y) ]]; then
-            return 0
-        fi
-        return 1
-    }
-
-    # Handle errors, clean up and exit
-    bdb_handle_error() {
-        local err_value="$?"         # Last command exit value
-        local err_command="$BASH_COMMAND" # Last command string
-        local err_message="${1:-UNKNOWN}" # Optional error message
-        bdb_error "Command: <${err_command}>"
-        bdb_error "Value: [${err_value}]"
-        bdb_error "Message: '${err_message}'"
-        bdb_error "Exiting now!"
-        printf "\n"
-        exit "${err_value}"
-    }
-
-    # Trap errors and call handler
-    trap 'bdb_handle_error' ERR
 
     # --- Bootstrap process ---
 
@@ -226,4 +156,4 @@ global_bootstrap() {
 }
 
 # --- Start bootstrap ---
-global_bootstrap
+bdb_bootstrap
