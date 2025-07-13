ce705819ffa02cf61ff9386801aa5f5795eff34d
norville <ing.norville@protonmail.com>
2025-07-13 08:03:54 +0000

    try sourcing helpers from bootstrap

--- dot_config/bdb/bdb_bootstrap.sh
+++ dot_config/bdb/bdb_bootstrap.sh
@@ -3,85 +3,20 @@
 
 ### BDF BOOT - bootstrap Bassa's Dot Files
 
-# Escape sequences for colored output (ANSI C quoting)
-RST_COL=$'\033[39;49;00m'   # reset
-RED_COL=$'\033[31;01m'      # red
-GRN_COL=$'\033[32;01m'      # green
-BLU_COL=$'\033[34;01m'      # blue
-YLW_COL=$'\033[33;01m'      # yellow
-CYN_COL=$'\033[36;01m'      # cyan
-MGN_COL=$'\033[35;01m'      # magenta
-WHT_COL=$'\033[97;01m'      # white
+#  --- Source helper functions (portable, works regardless of current directory) ---
 
-# --- Output helper functions ---
-
-# Print success message (green)
-bdb_success() {
-    printf "\n%s|vvv| SUCCESS %s. %s" "${GRN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print error message (red)
-bdb_error() {
-    printf "\n%s|xxx| ERROR %s %s" "${RED_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print alert message (yellow)
-bdb_alert() {
-    printf "\n%s|!!!| %s %s" "${YLW_COL}" "$1" "${RST_COL}"
-}
-
-# Print info message - begin (white)
-bdb_info_in() {
-    printf "%s\n|BDB| +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n|BDB|\n|BDB| %s.\n|BDB|%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print info message (white)
-bdb_info() {
-    printf "%s\n|BDB|\n|BDB| %s.\n|BDB|%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print info message - end (white)
-bdb_info_out() {
-    printf "%s\n|BDB|\n|BDB| %s.\n|BDB|\n|BDB| ---------------------------------------------------------------------------------%s" "${WHT_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print run message (blue)
-bdb_run() {
-    printf "\n%s|###| %s...%s" "${BLU_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print outcome message (cyan)
-bdb_outcome() {
-    printf " %s%s.%s" "${CYN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Print message before command (cyan)
-bdb_command() {
-    printf "\n%s|>>>| %s:%s" "${CYN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-}
-
-# Ask user yes/no, return 0 for yes (default: no)
-bdb_ask() {
-    printf "%s\n|???|\n|???| %s ? [y|N] >\n|???| %s" "${MGN_COL}" "${1:-*** UNKNOWN ***}" "${RST_COL}"
-    read -s -r -n 1 response
-    if [[ ${response:-} =~ (y|Y) ]]; then
-        return 0
-    fi
-    return 1
-}
-
-# Handle errors, clean up and exit
-bdb_handle_error() {
-    local err_value="$?"         # Last command exit value
-    local err_command="$BASH_COMMAND" # Last command string
-    local err_message="${1:-UNKNOWN}" # Optional error message
-    bdb_error "Command: <${err_command}>"
-    bdb_error "Value: [${err_value}]"
-    bdb_error "Message: '${err_message}'"
-    bdb_error "Exiting now!"
-    printf "\n"
-    exit "${err_value}"
-}
+BDB_HELPERS="https://raw.githubusercontent.com/norville/dotfiles/test/dot_config/bdb/bdb_helpers.sh"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if command -v curl >/dev/null 2>&1; then
+    curl -fsSL "${BDB_HELPERS}" -o "${SCRIPT_DIR}/bdb_helpers.sh"
+elif command -v wget >/dev/null 2>&1; then
+    wget -qO "${SCRIPT_DIR}/bdb_helpers.sh" "${BDB_HELPERS}"
+else
+    echo "Error: curl or wget is required to download helper scripts." >&2
+    exit 1
+fi
+# shellcheck disable=SC1091
+source "${SCRIPT_DIR}/bdb_helpers.sh"
 
 # Define main function to ensure download of entire script
 bdb_bootstrap() {
